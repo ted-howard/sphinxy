98527a4f8068d697a661c0c80821878390c486cc
ted-howard <thoward@esri.com>
2023-04-20 15:17:38 -0600

    doc string add

--- src/sphinxy/riddle.py
+++ src/sphinxy/riddle.py
@@ -8,6 +8,7 @@ class Riddle:
     answer: str
 
     def check_answer(self, answer: str) -> bool:
+        """this is check_answer"""
         return answer.lower() == self.answer.lower()
 
     def get_hint(self) -> Iterator[str]:
--- src/sphinxy/sphinx.py
+++ src/sphinxy/sphinx.py
@@ -27,6 +27,14 @@ class Sphinx:
         return "I have updated my riddle. Are you ready to solve it?"
 
     def pose_riddle(self, include_hint: bool = False) -> tuple[str, str | None]:
+        """Evaluates the given answer to the riddle.
+
+        Args:
+            include_hint (bool): The given answer to the riddle
+
+        Returns:
+            tuple: The result of the evaluation of the answer.
+        """
         hint = (
             f"Hint: The answer starts with the letter '{self._riddle.get_hint()}'."
             if include_hint
@@ -35,6 +43,19 @@ class Sphinx:
         return (self._riddle.question, hint)
 
     def check_riddle_answer(self, answer: str, return_hint: bool = False) -> str:
+        """Evaluates the given answer to the riddle.
+
+        Args:
+            answer (str): The given answer to the riddle.
+            return_hint (bool, optional): Controls whether a hint for the riddle should
+                be returned. Defaults to False.
+
+        Raises:
+            IncorrectAnswer: Exception for incorrect answer.
+
+        Returns:
+            str: The result of the evaluation of the answer.
+        """
         if self._riddle.check_answer(answer):
             return "Your answer was correct. You may pass."
         elif return_hint:
